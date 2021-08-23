4f0d3b3670b30e66f93b04fdb1dbcee6a0147b76
electrovir <electrovir@users.noreply.github.com>
2021-08-23 07:47:27 -0600

    add more configs

--- src/cli/config/configs.ts
+++ src/cli/config/configs.ts
@@ -2,19 +2,29 @@ import {readFile} from 'fs-extra';
 import {join, posix, sep} from 'path';
 import {extendedConfigsDir, virmatorRootDir} from '../../virmator-repo-paths';
 
-export enum ConfigFile {
-    Cspell = '.cspell.json',
+/** These config files are not used by any virmator commands but they are still helpful. */
+export enum BareConfigFile {
     GitAttributes = '.gitattributes',
     GitIgnore = '.gitignore',
+    GitHubActionsTest = '.github/workflows/virmator-tests.yml',
     NpmIgnore = '.npmignore',
-    Prettier = '.prettierrc.js',
-    TsConfig = 'tsconfig.json',
     VsCodeSettings = '.vscode/settings.json',
 }
+
+/** These are config files used by virmator commands */
+export enum CommandConfigFile {
+    Cspell = '.cspell.json',
+    Prettier = '.prettierrc.js',
+    TsConfig = 'tsconfig.json',
+}
+
+export const ConfigFile = {...CommandConfigFile, ...BareConfigFile};
+export type ConfigFile = CommandConfigFile | BareConfigFile;
 
 export const extendableConfigFile = {
     [ConfigFile.Prettier]: '.prettierrc-base.js',
     [ConfigFile.Cspell]: '.cspell-base.json',
+    [ConfigFile.TsConfig]: 'tsconfig-base.json',
 } as const;
 
 export async function readVirmatorVersionOfConfigFile(
