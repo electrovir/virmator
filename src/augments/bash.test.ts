2d030e1197b57a4020f1bc2969464cfae5ac88b9
electrovir <electrovir@users.noreply.github.com>
2021-09-13 18:03:43 -0600

    add bash test that fails buffer overflow

--- src/augments/bash.test.ts
+++ src/augments/bash.test.ts
@@ -1,6 +1,8 @@
+import {join} from 'path';
 import {testGroup} from 'test-vir';
+import {virmatorRootDir} from '../file-paths/virmator-repo-paths';
 import {runBashCommand} from './bash';
-import {toPosixPath} from './string';
+import {interpolationSafeWindowsPath, toPosixPath} from './string';
 
 testGroup({
     description: runBashCommand.name,
@@ -26,6 +28,22 @@ testGroup({
             },
             test: async () => {
                 return await runBashCommand('pwd', __dirname);
+            },
+        });
+
+        runTest({
+            description: 'no buffer overflow errors',
+            expect: undefined,
+            test: async () => {
+                const packageLockPath = interpolationSafeWindowsPath(
+                    join(virmatorRootDir, 'package-lock.json'),
+                );
+
+                const commandOutput = await runBashCommand(
+                    `seq 20 | xargs -I{} cat ${packageLockPath}`,
+                );
+
+                return commandOutput.error;
             },
         });
     },
