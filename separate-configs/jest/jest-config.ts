e654c09f3b64cb70f59683768bc3e02ba44ea450
electrovir <electrovir@users.noreply.github.com>
2022-03-20 18:28:44 -0600

    try passing posix path to jest

--- separate-configs/jest/jest-config.ts
+++ separate-configs/jest/jest-config.ts
@@ -1,20 +1,20 @@
 import {join} from 'path';
 import {InitialOptionsTsJest} from 'ts-jest';
 
-const projectRoot = process.cwd();
+const cwd = process.cwd();
 
 const config: InitialOptionsTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: false,
-    rootDir: projectRoot,
+    rootDir: cwd,
     silent: false,
     modulePathIgnorePatterns: ['.*.type.test.ts$'],
-    roots: [join(projectRoot, 'src')],
+    roots: [join(cwd, 'src')],
     setupFilesAfterEnv: [join(__dirname, 'jest-setup.ts')],
     globals: {
         'ts-jest': {
-            tsconfig: join(projectRoot, 'tsconfig.json'),
+            tsconfig: join(cwd, 'tsconfig.json'),
             diagnostics: {
                 warnOnly: true,
                 ignoreCodes: ['TS151001'],
