b0e76a9db52edc59c096a139600bbf77448c3088
electrovir <electrovir@users.noreply.github.com>
2022-03-21 21:38:54 -0600

    bump test timeout again (for GitHub Actions)

--- src/jest/jest-config.ts
+++ src/jest/jest-config.ts
@@ -3,7 +3,8 @@ import {virmatorJestConfig} from '../exportable-jest-config/jest-config';
 
 const virmatorInternalTestingConfig: InitialOptionsTsJest = {
     ...virmatorJestConfig,
-    testTimeout: 30000,
+    // this insanely high timeout is for GitHub Actions, which takes an eternity to run
+    testTimeout: 60000,
     modulePathIgnorePatterns: [
         ...virmatorJestConfig.modulePathIgnorePatterns!,
         'test-files',
