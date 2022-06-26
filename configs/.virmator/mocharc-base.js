d63612565e35606b1eead28ca305df72901cebf3
electrovir <electrovir@users.noreply.github.com>
2022-06-26 20:01:00 +0000

    allow testing single files at a time

--- configs/.virmator/mocharc-base.js
+++ configs/.virmator/mocharc-base.js
@@ -1,9 +1,14 @@
+function isTestFile(arg) {
+    return arg.match(/\.tsx?$/);
+}
+const allTestFiles = 'src/**/*.test.ts?(x)';
+
 /** @type {import('mocha').MochaOptions} */
 const mochaConfig = {
     checkLeaks: true,
     color: true,
     require: 'ts-node/register',
-    spec: 'src/**/*.test.ts',
+    ...(process.argv.some(isTestFile) ? {} : {spec: allTestFiles}),
 };
 
 module.exports = mochaConfig;
