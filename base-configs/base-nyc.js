efed169026e2e0a6518a9a85446d211f67e70ee8
electrovir <electrovir@users.noreply.github.com>
2022-10-10 11:22:43 +0000

    update configs

--- base-configs/base-nyc.js
+++ base-configs/base-nyc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const baseConfig = {
-    /** Require 100% test coverage */
+    /** Run thresholds on each individual file */
     perFile: 100,
     /** Caches cause problems. */
     cache: false,
@@ -15,6 +15,12 @@ const baseConfig = {
     /** Don't show files that meet 100% test coverage. */
     skipFull: true,
     tempDir: './node_modules/.nyc-output/',
+
+    /** Require 100% test coverage in all things */
+    branches: 100,
+    functions: 100,
+    lines: 100,
+    statements: 100,
 };
 
 module.exports = baseConfig;
