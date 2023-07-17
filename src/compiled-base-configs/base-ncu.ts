1de4dcb516ff3981f017d70edb41a9a45565a017
electrovir <electrovir@users.noreply.github.com>
2023-07-17 14:43:58 +0000

    narrow types for base-ncu.ts

--- src/compiled-base-configs/base-ncu.ts
+++ src/compiled-base-configs/base-ncu.ts
@@ -1,8 +1,9 @@
 import {RunOptions} from 'npm-check-updates';
+import {ReadonlyDeep} from 'type-fest';
 
-export const baseNcuConfig: RunOptions = {
+export const baseNcuConfig = {
     reject: [
         /** Not ready for v3. */
         'prettier',
     ],
-};
+} as const satisfies ReadonlyDeep<RunOptions>;
