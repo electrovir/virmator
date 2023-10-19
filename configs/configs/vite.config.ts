e2a1d95b8c8a3738069a58942f91df661f753086
electrovir <electrovir@users.noreply.github.com>
2023-10-19 22:14:17 +0000

    make default vite config easier to configure

--- configs/configs/vite.config.ts
+++ configs/configs/vite.config.ts
@@ -1,5 +1,7 @@
 import {defineConfig} from 'virmator/dist/compiled-base-configs/base-vite';
 
 export default defineConfig({forGitHubPages: true}, (baseConfig) => {
-    return baseConfig;
+    return {
+        ...baseConfig,
+    };
 });
