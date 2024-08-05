996416a8fc6afa29cd1848d6a2978077b50617bd
electrovir <electrovir@users.noreply.github.com>
2024-08-05 15:43:53 +0000

    [minor] add publish version log

--- packages/publish/src/publish.ts
+++ packages/publish/src/publish.ts
@@ -141,6 +141,8 @@ export const virmatorPublishPlugin = defineVirmatorPlugin(
                 });
             }
 
+            log.info(`Publishing version ${nextVersion}...`);
+
             await updateVersions(nextVersion, monoRepoRootPath, monoRepoPackages, log);
 
             await runHiddenShellCommand('npm i');
