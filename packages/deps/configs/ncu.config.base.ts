3407a195ff02ac5ec4643ec5185fa1766a72a704
electrovir <electrovir@users.noreply.github.com>
2025-10-26 15:42:50 +0000

    [patch] unblock eslint-plugin-unicorn upgrades

--- packages/deps/configs/ncu.config.base.ts
+++ packages/deps/configs/ncu.config.base.ts
@@ -33,8 +33,6 @@ export const baseNcuConfig = {
     reject: [
         /** 3.4 is broken https://github.com/prettier/prettier/issues/16936 */
         'prettier',
-        /** https://github.com/sindresorhus/eslint-plugin-unicorn/pull/2706 */
-        'eslint-plugin-unicorn',
     ],
     deprecated: false,
     /** Returns days. */
